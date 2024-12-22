e01050b6cec767cdd46df8fe215c02941f0166ad
Maty Pierník <maty.piernik@gmail.com>
2024-12-22 23:58:42 +0100

    Verze 0.4

---
+++ static/js/script.js
@@ -0,0 +1,10 @@
+var map = L.map('map').setView([49.981, 16.373], 8);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+L.marker([50.083, 14.417]).addTo(map)
+    .bindPopup('Test: Praha')
+    .openPopup();
