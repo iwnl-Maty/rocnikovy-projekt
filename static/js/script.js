b745c2b255218f4a93b6fa31366f9d54b1c2bcf4
Maty Pierník <maty.piernik@gmail.com>
2024-12-24 18:01:11 +0100

    Verze 0.6.2

--- static/js/script.js
+++ static/js/script.js
@@ -5,6 +5,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; OpenStreetMap contributors'
 }).addTo(map);
 
-L.marker([50.083, 14.417]).addTo(map)
-    .bindPopup('Testovací bod: Praha')
-    .openPopup();
+const points = [
+    { name: "Praděd", coords: [50.083, 17.230], description: "Nejvyšší vrchol Jeseníků." },
+    { name: "Šerák", coords: [50.170, 17.157], description: "Oblíbený turistický cíl s krásnými výhledy." },
+    { name: "Karlova Studánka", coords: [49.790, 17.125], description: "Známé lázeňské město." },
+    { name: "Rejvíz", coords: [50.244, 17.232], description: "Největší rašeliniště ve Slezsku." }
+];
+
+points.forEach(point => {
+    L.marker(point.coords).addTo(map)
+        .bindPopup(`<b>${point.name}</b><br>${point.description}`);
+});
+
+setTimeout(function () {
+        map.invalidateSize();
+    }, 100);
