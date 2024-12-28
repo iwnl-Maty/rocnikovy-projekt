84c3e33f1cf6345abb0bad9f6785bdccae0e3bd0
Maty Pierník <maty.piernik@gmail.com>
2024-12-28 02:46:30 +0100

    Verze 0.8.3

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template # type: ignore
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
-if __name__ == '__main__':
+@app.route('/detail')
+def detail():
+    name = request.args.get('name', 'Neznámé místo')
+    description = request.args.get('description', 'Bez popisu')
+    return render_template('detail.html', name=name, description=description)
+
+if __name__ == "__main__":
     app.run(debug=True)
