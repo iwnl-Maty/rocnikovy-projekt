b3586acd53eb3104d420354abe2965d2e0ecf5f3
Maty Pierník <maty.piernik@gmail.com>
2024-12-29 23:46:00 +0100

    Verze 0.9.1

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/detail')
+def detail():
+    name = request.args.get('name', 'Neznámé místo')
+    description = request.args.get('description', 'Bez popisu')
+    return render_template('detail.html', name=name, description=description)
+
 if __name__ == "__main__":
     app.run(debug=True)
