62fdd9fa24cca57fe8d8a527f5cec8742eaed684
Maty Pierník <maty.piernik@gmail.com>
2025-01-13 22:50:51 +0100

    verze predposledni

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash, jsonify  # type: ignore
+from flask import Blueprint, render_template, redirect, url_for, request, flash  # type: ignore
 from flask_login import login_user, logout_user, login_required, current_user  # type: ignore
 from .models import User, PointOfInterest
 from . import db
@@ -7,13 +7,13 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    points = PointOfInterest.query.all()
-    points_dict = [{"id": p.id, "name": p.name, "description": p.description, "latitude": p.latitude, "longitude": p.longitude} for p in points]
+    points = PointOfInterest.query.all()  # Načtení všech bodů zájmu z databáze
+    points_dict = [point.to_dict() for point in points]  # Serializace bodů do JSON formátu
     return render_template('index.html', points=points_dict)
 
 @main.route('/detail/<int:point_id>')
 def detail(point_id):
-    point = PointOfInterest.query.get_or_404(point_id)
+    point = PointOfInterest.query.get_or_404(point_id)  # Načtení bodu zájmu podle ID
     return render_template('detail.html', point=point)
 
 @main.route('/add', methods=['GET', 'POST'])
@@ -25,16 +25,48 @@ def add_point():
         latitude = request.form.get('latitude')
         longitude = request.form.get('longitude')
 
-        new_point = PointOfInterest(
-            name=name,
-            description=description,
-            latitude=float(latitude),
-            longitude=float(longitude),
-            added_by=current_user.id
-        )
-        db.session.add(new_point)
-        db.session.commit()
-        flash('Bod zájmu byl přidán!')
-        return redirect(url_for('main.index'))
+        try:
+            new_point = PointOfInterest(
+                name=name,
+                description=description,
+                latitude=float(latitude),
+                longitude=float(longitude),
+                added_by=current_user.id  # Získání ID aktuálního uživatele
+            )
+            db.session.add(new_point)
+            db.session.commit()
+            flash('Bod zájmu byl přidán!')
+            return redirect(url_for('main.index'))
+        except Exception as e:
+            db.session.rollback()
+            flash(f'Chyba při přidávání bodu: {str(e)}')
 
     return render_template('add_point.html')
+
+@main.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:  # Jednoduchá kontrola hesla (doporučujeme hashování)
+            login_user(user)
+            flash('Přihlášení proběhlo úspěšně.')
+            return redirect(url_for('main.index'))
+        else:
+            flash('Špatné přihlašovací údaje.')
+
+    return render_template('login.html')
+
+@main.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Odhlášení proběhlo úspěšně.')
+    return redirect(url_for('main.index'))
+
+@main.app_errorhandler(404)
+def page_not_found(error):
+    """Ošetření chyby 404 pro případ, že URL neexistuje."""
+    return render_template('404.html'), 404
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -5,41 +5,36 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
   attribution: "&copy; OpenStreetMap contributors",
 }).addTo(map);
 
-const points = [
-  {
-    id: 1,
-    name: "Praděd",
-    coords: [50.083, 17.23],
-    description: "Nejvyšší vrchol Jeseníků.",
-    image: "static/images/praded.png",
-  },
-  {
-    id: 2,
-    name: "Šerák",
-    coords: [50.17, 17.157],
-    description: "Oblíbený turistický cíl s krásnými výhledy.",
-    image: "static/images/serak.png",
-  },
-  {
-    id: 3,
-    name: "Karlova Studánka",
-    coords: [49.79, 17.125],
-    description: "Známé lázeňské město.",
-    image: "static/images/studanka.png",
-  },
-  {
-    id: 4,
-    name: "Rejvíz",
-    coords: [50.244, 17.232],
-    description: "Největší rašeliniště ve Slezsku.",
-    image: "static/images/rejviz.png",
-  },
-];
-
+// Iterate through points passed from Flask via template
 points.forEach((point) => {
-  L.marker(point.coords).addTo(map).bindPopup(`
-    <b>${point.name}</b><br>
-    ${point.description}<br>
-    <a href='/detail/${point.id}'>Více informací</a>
-  `);
+  if (point.id) {
+    L.marker([point.latitude, point.longitude]).addTo(map).bindPopup(`
+      <b>${point.name}</b><br>
+      ${point.description}<br>
+      <a href='/detail/${point.id}'>Více informací</a>
+    `);
+  } else {
+    console.error("Chybí ID pro bod:", point);
+  }
+});
+
+document.getElementById("search").addEventListener("input", function (e) {
+  const query = e.target.value.toLowerCase();
+  let found = false;
+
+  points.forEach((point) => {
+    if (point.name.toLowerCase().includes(query)) {
+      map.flyTo([point.latitude, point.longitude], 12);
+      found = true;
+
+      L.popup()
+        .setLatLng([point.latitude, point.longitude])
+        .setContent(`<b>${point.name}</b><br>${point.description}`)
+        .openOn(map);
+    }
+  });
+
+  if (!found && query !== "") {
+    alert("Žádný bod zájmu nebyl nalezen!");
+  }
 });
